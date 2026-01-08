ea6f3eb429fcd47b102a25fb32ecb467c7b87d2d
Shaikh Noor Abbas <shaikhnoor1512@gmail.com>
2026-01-08 11:05:24 +0530

    added ant design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
+import { Button } from "antd";
 export default function App() {
   return (
     <>
       <h1 className="underline">Heading One</h1>
       <p className="text-sm">this is a Sample sentence</p>
+      <Button type="primary">Click Me</Button>
     </>
   );
 }
