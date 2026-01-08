d3b4620f012a2b8fbee516d426a746bff994ae25
Shaikh Noor Abbas <shaikhnoor1512@gmail.com>
2026-01-08 11:45:39 +0530

    added heading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
+import { PlusOutlined } from "@ant-design/icons";
 import { Button } from "antd";
 export default function App() {
   return (
     <>
-      <h1 className="underline">Heading One</h1>
-      <p className="text-sm">this is a Sample sentence</p>
-      <Button type="primary">Click Me</Button>
+      <section className="min-h-screen bg-rose-100 flex flex-col items-center md:p-4">
+        <div className="flex justify-between items-center bg-blue-600 w-10/12 my-5 p-5">
+          <h1 className="capitalize text-center text-white text-2xl md:text-5xl font-bold">
+            CRUD Operation
+          </h1>
+          <Button
+            shape="circle"
+            icon={<PlusOutlined />}
+            size="large"
+            type="text"
+            className="!text-black !bg-green-300 hover:!bg-white hover:!scale-112"
+          />
+        </div>
+      </section>
     </>
   );
 }
